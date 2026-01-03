9399776b24efb5064558fbd0c67989c377183d9d
Patryk0v <93556777+Patryk0v@users.noreply.github.com>
2026-01-03 20:36:35 +0100

    Day 2 – first pandas analysis

--- scripts/day2_data_loading.py
+++ scripts/day2_data_loading.py
@@ -1,1 +1,20 @@
+import pandas as pd
+
+# wczytanie danych
+df = pd.read_csv("../data/sample_data.csv")
+
+print("DANE:")
+print(df)
+
+# obliczenia
+df["total_value"] = df["price"] * df["quantity"]
+
+summary = df.groupby("category")["total_value"].sum()
+
+print("\nPODSUMOWANIE:")
+print(summary)
+
+# zapis wyniku
+summary.to_csv("../data/summary_by_category.csv")
+print("\nZapisano summary_by_category.csv")
 
